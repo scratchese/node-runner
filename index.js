077e2ea3c9fb896a27ce8b9541774df17387e5b8
amazingandyyy <andycandomore@gmail.com>
2019-08-15 10:00:42 -0700

    try git-hooks

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cwd = process.cwd()
 const target = process.argv[2] || require(path.resolve(cwd, 'package.json')).main
 const file = path.resolve(cwd, target)
 
-let session = launch();
+let session = launch()
 
 function launch () {
   console.log(`\x1b[32m[node-run] node ${target}`, '\x1b[0m')
@@ -24,7 +24,7 @@ process.on('SIGINT', () => {
 
 fs.watch(cwd, (curr, filename) => {
   if (filename.indexOf('node_module') < 0) {
-    session.kill('SIGINT');
-    session = launch();
+    session.kill('SIGINT')
+    session = launch()
   }
 })
